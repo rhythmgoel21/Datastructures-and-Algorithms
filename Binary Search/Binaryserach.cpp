6568194e4f71110c9748a6b2313883316bc1b905
Rhythm Goel <78782767+rhythmgoel21@users.noreply.github.com>
2022-11-26 13:45:36 +0530

    Create Binaryserach.cpp

---
+++ Binary Search/Binaryserach.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0,e=nums.size()-1,mid;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            if(nums[mid]<target)
+            {
+                s=mid+1;
+            }
+        }
+        return -1;
+    }
+};
